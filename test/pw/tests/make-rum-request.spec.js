a77b0897ae4417e09f6eb597c76b5d4bbe1088f7
David Bosschaert <bosschae@adobe.com>
2024-09-17 14:26:42 +0200

    Initial stuff for e2e tests

---
+++ test/pw/tests/make-rum-request.spec.js
@@ -0,0 +1,30 @@
+const { test, expect } = require('@playwright/test');
+const { execSync } = require('child_process');
+
+function getTestID() {
+  let { RUM_TEST_ID } = process.env;
+  if (!RUM_TEST_ID) {
+    RUM_TEST_ID = Math.random().toString(36).substring(8);
+  }
+  console.log('Using RUM_TEST_ID', RUM_TEST_ID);
+  return RUM_TEST_ID;
+}
+
+
+test('Make a request that triggers a RUM request', async ({ page }) => {
+  const testID = getTestID();
+  await page.goto(`https://main--rum-integrations--adobe.aem.live/?rum=on&test_rum_id=${testID}`);
+  await expect(page.getByText('Boilerplate Highlights')).toBeVisible();
+
+  // Wait 5 seconds for the rum request to appear at the backend
+  await page.waitForTimeout(5000);
+
+  const cmdline =
+    `bq query --format json --nouse_legacy_sql 'SELECT count(*) FROM \`helix-225321.helix_rum.cluster\`
+    WHERE CONTAINS_SUBSTR(url, "rum-integrations") AND id = "${testID}" LIMIT 10' | jq ".[]|.[]"`;
+  console.log('Executing: ', cmdline);
+
+  let result = execSync(cmdline);
+  console.log('Result:', result.toString());
+// jq ".[]|.[]
+});
