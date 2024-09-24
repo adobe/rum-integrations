bfe681caa8551bd5ea846f73cc89095510b3100e
David Bosschaert <bosschae@adobe.com>
2024-09-24 16:47:00 +0100

    Make Cloudflare sleep longer

--- src/scripts/get_extra_sleep_for_worker.sh
+++ src/scripts/get_extra_sleep_for_worker.sh
@@ -3,7 +3,7 @@ WORKER=$1
 if [ "$WORKER" = "FASTLY" ]; then
     echo "Fastly doesn't need extra sleep"
 else
-    echo Sleep an extra hour for Cloudflare or potential Cloudflare
-    sleep 3600
+    echo Sleep an extra 90 minutes for Cloudflare or potential Cloudflare
+    sleep 5400
 fi
 
