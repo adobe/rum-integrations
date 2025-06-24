9c281e67d6eeb810dc4f25bae5deb74c039a61f8
David Bosschaert <bosschae@adobe.com>
2025-06-24 12:52:02 +0100

    Increase s3 sleep

--- src/scripts/get_extra_sleep_for_worker.sh
+++ src/scripts/get_extra_sleep_for_worker.sh
@@ -1,9 +1,16 @@
 WORKER=$1
+TASK=$2
 
 if [ "$WORKER" = "FASTLY" ]; then
     echo "Fastly doesn't need extra sleep"
 else
-    echo Sleep an extra 90 minutes for Cloudflare or potential Cloudflare
-    sleep 5400
+    echo "Running with Cloudflare or potential Cloudflare worker"
+    if [ "$TASK" = "s3" ]; then
+        echo "Cloudflare s3 extra sleep"
+        sleep 2700
+    else
+        echo "Cloudflare extra sleep"
+        sleep 5400
+    fi
 fi
 
